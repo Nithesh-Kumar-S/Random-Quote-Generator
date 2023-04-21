c7a74fa0a3e4c7ccb40fedc2ac63036428b66fd2
Nithesh-Kumar-S <108716767+Nithesh-Kumar-S@users.noreply.github.com>
2023-04-21 18:56:37 +0530

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,36 @@
+const quotes = [
+    {
+      quote: "And now that you don't have to be perfect, you can be good.",
+      author: "John Steinbeck"
+    },
+    {
+      quote: "Don't let your happiness depend on something you may lose.",
+      author: "C.S. Lewis"
+    },
+    {
+      quote: "Straight roads do not make skillful drivers.",
+      author: "Paula Coelho"
+    },
+    {
+      quote: "When you can't change the direction of the wind, adjust your sails.",
+      author: "​H. Jackson Brown Jr"
+    },
+    {
+      quote: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"
+    },
+    {
+      quote: "To live is the rarest thing in the world. Most people exist, that is all.",
+      author: "Oscar Wilde"
+    },
+    {
+      quote: "Pain is inevitable. Suffering is optional.",
+      author: "Haruki Murakami"
+    },
+    {
+        quote: "Anything worth dying for is certainly worth living for.",
+        author: "Joseph Heller"
+      },
+  ];
+  
+  export default quotes;
